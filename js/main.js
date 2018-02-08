b73ef583ae4e01c70b7a4c7eebf7e3c543c709ac
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-08 12:59:08 -0500

    focus on first text field

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('linked');
+// Treehouse Techdegree Project 3: Build an Interactive Form
+
+// I am aiming for an "Exceeds Expectations" grade with this project.
+
+// Make sure the DOM loads first
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    // focus on the first text field, "name"
+    $('#name').focus();
+
+
+});
