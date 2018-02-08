2abe16ceb04e3cc87a4ca342d22832a4638be134
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-08 13:56:17 -0500

    textarea box created, functional - needs styling

--- js/main.js
+++ js/main.js
@@ -8,5 +8,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // focus on the first text field, "name"
     $('#name').focus();
 
+    // if "other is clicked in 'Job Role' field, a text entry box opens
+    // an event handler for selecting "other"
+    $('#title').on('change', (event) => {
+
+        const option = event.target;
+
+        // check to see if the "other" option was clicked,
+        if (option.value === "other") {
+            // if the textarea box already exists, do nothing
+            if (!(document.getElementById('other-title'))) {
+                let textarea = document.createElement('textarea')
+                $(textarea).attr('id', 'other-title').attr('name', 'user_other_title');
+                option.after(textarea);
+            }
+        } else { // a different option was selected, so remove the 
+            //textarea box if it exists
+            if (document.getElementById('other-title')) {
+                let textarea = document.getElementById('other-title')
+                textarea.parentNode.removeChild(textarea);
+            }
+        }
+    });
 
 });
