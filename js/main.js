66bfded3dc2d18615d7337791550f989b7174455
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-02-08 21:47:24 -0500

    t-shirt colors selection functional

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@
 // Make sure the DOM loads first
 document.addEventListener('DOMContentLoaded', (event) => {
 
+    const $colorDiv = $('#colors-js-puns');
+    const $colorOptions = $('#color').children(); // reference list to the options in the color <select> box
+
     // focus on the first text field, "name"
     $('#name').focus();
 
@@ -33,19 +36,55 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     // only display the color option if a selection is made in design drop down menu
     // hide the color selection area on page load
-    $('#colors-js-puns').hide();
+    $colorDiv.hide();
+
+
+    // change all the options in the color div <select> box to include class attributes
+    // and remove the extra text in brackets from the content
+    // so we can use the class attributes to show / hide the options
+    for (let i = 0; i < $colorOptions.length; i++) {
+        let option = $colorOptions[i];
+        if ($(option).text().toLowerCase().includes('js puns')) {
+            $(option).addClass('color_opt_puns');
+            $(option).text($(option).text().replace(' (JS Puns shirt only)', ''));
+        }
+        // replace does not work here, as the heart character (&#9829) does not register
+        // so I'm using .substring() to remove the extra text
+        if ($(option).text().toLowerCase().includes('js shirt')) {
+            $(option).addClass('color_opt_heart');
+            let optionText = $(option).text();
+            // find where the bracket starts, the index should be 1 space before that
+            // because the space should also be removed
+            let indexOfBracket = optionText.indexOf('(') - 1;
+            optionText = optionText.substring(0, indexOfBracket);
+            $(option).text(optionText);
+
+        }
+    }
+
     // add an event handler on the design menu
+    // to show and hide the color selection options
     $('#design').on('change', (event) => {
         const option = event.target;
 
+        // hide the color div, in case "Select Theme" is re-selected from the box
+        $colorDiv.hide();
+
         if (option.value === "js puns") {
-            $('#colors-js-puns').show();
-
-        } else if (option.value === "heart js") {
-            $('#colors-js-puns').show();
-
-        } else { // else hide the color options again!
-            $('#colors-js-puns').hide();
+            // retrieve the first option with puns
+            const firstOptVal = $('.color_opt_puns').first().val();
+            $('#color').val(firstOptVal).change();
+            $('.color_opt_puns').show();
+            $('.color_opt_heart').hide();
+            $colorDiv.show();
+        }
+        if (option.value === "heart js") {
+            // retrieve the first option with a heart
+            const firstOptVal = $('.color_opt_heart').first().val();
+            $('#color').val(firstOptVal).change();
+            $('.color_opt_puns').hide();
+            $('.color_opt_heart').show();
+            $colorDiv.show();
         }
 
     });
